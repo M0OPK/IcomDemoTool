a4865aad6c1626bf56a4f416392e25b0b5fac516
M0OPK <peter@m0opk.org>
2016-04-12 23:47:09 +0100

    Initial commit. Tests auto detect.

---
+++ IcomTool/IcomDemo.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace IcomTool
+{
+    partial class IcomDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbPorts = new System.Windows.Forms.ListBox();
+            this.lbRadios = new System.Windows.Forms.ListBox();
+            this.cmdConnect = new System.Windows.Forms.Button();
+            this.mlText = new System.Windows.Forms.TextBox();
+            this.cmdSend = new System.Windows.Forms.Button();
+            this.cmdFind = new System.Windows.Forms.Button();
+            this.cmdDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbPorts
+            // 
+            this.lbPorts.FormattingEnabled = true;
+            this.lbPorts.Location = new System.Drawing.Point(26, 35);
+            this.lbPorts.Name = "lbPorts";
+            this.lbPorts.Size = new System.Drawing.Size(117, 69);
+            this.lbPorts.TabIndex = 0;
+            // 
+            // lbRadios
+            // 
+            this.lbRadios.FormattingEnabled = true;
+            this.lbRadios.Location = new System.Drawing.Point(167, 35);
+            this.lbRadios.Name = "lbRadios";
+            this.lbRadios.Size = new System.Drawing.Size(126, 69);
+            this.lbRadios.TabIndex = 1;
+            // 
+            // cmdConnect
+            // 
+            this.cmdConnect.Location = new System.Drawing.Point(26, 111);
+            this.cmdConnect.Name = "cmdConnect";
+            this.cmdConnect.Size = new System.Drawing.Size(117, 21);
+            this.cmdConnect.TabIndex = 2;
+            this.cmdConnect.Text = "Connect";
+            this.cmdConnect.UseVisualStyleBackColor = true;
+            this.cmdConnect.Click += new System.EventHandler(this.cmdConnect_Click);
+            // 
+            // mlText
+            // 
+            this.mlText.Location = new System.Drawing.Point(26, 181);
+            this.mlText.Multiline = true;
+            this.mlText.Name = "mlText";
+            this.mlText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.mlText.Size = new System.Drawing.Size(267, 139);
+            this.mlText.TabIndex = 3;
+            // 
+            // cmdSend
+            // 
+            this.cmdSend.Location = new System.Drawing.Point(167, 138);
+            this.cmdSend.Name = "cmdSend";
+            this.cmdSend.Size = new System.Drawing.Size(126, 22);
+            this.cmdSend.TabIndex = 4;
+            this.cmdSend.Text = "Send";
+            this.cmdSend.UseVisualStyleBackColor = true;
+            this.cmdSend.Click += new System.EventHandler(this.cmdSend_Click);
+            // 
+            // cmdFind
+            // 
+            this.cmdFind.Location = new System.Drawing.Point(167, 109);
+            this.cmdFind.Name = "cmdFind";
+            this.cmdFind.Size = new System.Drawing.Size(126, 23);
+            this.cmdFind.TabIndex = 5;
+            this.cmdFind.Text = "Find Radio";
+            this.cmdFind.UseVisualStyleBackColor = true;
+            this.cmdFind.Click += new System.EventHandler(this.cmdFind_Click);
+            // 
+            // cmdDisconnect
+            // 
+            this.cmdDisconnect.Location = new System.Drawing.Point(26, 141);
+            this.cmdDisconnect.Name = "cmdDisconnect";
+            this.cmdDisconnect.Size = new System.Drawing.Size(117, 19);
+            this.cmdDisconnect.TabIndex = 6;
+            this.cmdDisconnect.Text = "Disconnect";
+            this.cmdDisconnect.UseVisualStyleBackColor = true;
+            this.cmdDisconnect.Click += new System.EventHandler(this.cmdDisconnect_Click);
+            // 
+            // IcomDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 332);
+            this.Controls.Add(this.cmdDisconnect);
+            this.Controls.Add(this.cmdFind);
+            this.Controls.Add(this.cmdSend);
+            this.Controls.Add(this.mlText);
+            this.Controls.Add(this.cmdConnect);
+            this.Controls.Add(this.lbRadios);
+            this.Controls.Add(this.lbPorts);
+            this.Name = "IcomDemo";
+            this.Text = "Icom Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbPorts;
+        private System.Windows.Forms.ListBox lbRadios;
+        private System.Windows.Forms.Button cmdConnect;
+        private System.Windows.Forms.TextBox mlText;
+        private System.Windows.Forms.Button cmdSend;
+        private System.Windows.Forms.Button cmdFind;
+        private System.Windows.Forms.Button cmdDisconnect;
+    }
+}
+
---
+++ IcomTool/IcomDemo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using CIVSharp;
+
+namespace IcomTool
+{
+    public partial class IcomDemo : Form
+    {
+        CIVSharp.CIV myCIV;
+        public IcomDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmdDisconnect.Enabled = false;
+            cmdSend.Enabled = false;
+            myCIV = new CIVSharp.CIV();
+            string[] radios = myCIV.GetRadioNames();
+            string[] ports = myCIV.GetSerialPorts();
+            foreach (string radio in radios)
+                lbRadios.Items.Add(radio);
+            foreach (string port in ports)
+                lbPorts.Items.Add(port);
+        }
+
+        private void cmdConnect_Click(object sender, EventArgs e)
+        {
+            myCIV.setRadioID(CIVSharp.CIV.Radio.IC_7100);
+            if (!myCIV.OpenSerialPort(lbPorts.SelectedItem.ToString(), 300))
+            {
+                string exceptionText = myCIV.GetSerialException().Message;
+                MessageBox.Show("Unable to open port " + lbPorts.SelectedItem.ToString() + ": " + exceptionText, "Error opening port: ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                myCIV.CommandWaiting += new CIVSharp.CIVCommandReadyEvent(CIVDataHandler);
+                cmdConnect.Enabled = false;
+                cmdFind.Enabled = false;
+                cmdDisconnect.Enabled = true;
+                cmdSend.Enabled = true;
+            }
+        }
+
+        private void CIVDataHandler(object sender, EventArgs e)
+        {
+            CIVSharp.CIV thisCIV = (CIVSharp.CIV)sender;
+
+            while (thisCIV.CommandQueued())
+            {
+                byte[] thisCommand = thisCIV.ReadCommand();
+                string hex = BitConverter.ToString(thisCommand);
+                this.Invoke((MethodInvoker)delegate
+                {
+                    mlText.AppendText(hex + "\r\n");
+                });
+            }
+        }
+
+        private void cmdSend_Click(object sender, EventArgs e)
+        {
+
+            //byte[] buffer = { 0x88, 0xE0, 0x19, 0x00 };
+            byte[] buffer = { (byte)CIVSharp.CIV.CommandBytes.COMMAND_TRANCEIVER_ID_READ, 0x00 };
+            myCIV.TransmitCommand(buffer);
+            //myCIV.AutoDetectrRadio();
+        }
+
+        private void cmdFind_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            CIV.RadioInfo radioInfo = myCIV.AutoDetectRadio(true);
+            Cursor.Current = Cursors.Default;
+            if (radioInfo.RadioID != CIV.Radio.NULL_RADIO)
+            {
+                myCIV.CommandWaiting += new CIVSharp.CIVCommandReadyEvent(CIVDataHandler);
+                cmdConnect.Enabled = false;
+                cmdFind.Enabled = false;
+                cmdDisconnect.Enabled = true;
+                cmdSend.Enabled = true;
+                MessageBox.Show("Found radio: " + radioInfo.RadioName + " on port " + radioInfo.CommPort + " with baud rate " + radioInfo.baudRate.ToString() + " with address " + radioInfo.RadioAddress.ToString("X2"), "Radio Autodetect", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void cmdDisconnect_Click(object sender, EventArgs e)
+        {
+            myCIV.CloseSerialPort();
+            cmdConnect.Enabled = true;
+            cmdFind.Enabled = true;
+            cmdDisconnect.Enabled = false;
+            cmdSend.Enabled = false;
+
+        }
+    }
+}
